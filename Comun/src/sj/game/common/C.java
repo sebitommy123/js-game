e5950cc583ed164ba1d445a6ad2e1ad1506daadd
Antonio Ruiz <antonio.ruiz@desenlace.net>
2017-01-13 19:03:31 +0100

    ip

--- Comun/src/sj/game/common/C.java
+++ Comun/src/sj/game/common/C.java
@@ -3,6 +3,6 @@ package sj.game.common;
 public class C {
 
 	public static final int PORT = 54321;
-	public static final String HOST = "192.168.1.133";
+	public static final String HOST = "192.168.1.106";
 	
 }
