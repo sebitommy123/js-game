956bf646459c93cb382643817217389be8571597
sebi <sebi.szafir@hotmail.com>
2017-01-13 18:52:04 +0100

    Threory (also test)

--- Cliente/src/sj/game/client/Main.java
+++ Cliente/src/sj/game/client/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
+		//sent output to server
+		
+		//create thread to wait for server inputs
+		
+		//run game
+		
 	}
 
 }
