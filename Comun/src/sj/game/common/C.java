265eba8fa6dc8e627a0bec33d48dd044b7f6d175
Jorge <jorgehuertatoledo@gmail.com>
2017-01-17 22:42:39 +0100

    no message

--- Comun/src/sj/game/common/C.java
+++ Comun/src/sj/game/common/C.java
@@ -1,9 +1,10 @@
 package sj.game.common;
 
 public class C {
+	public C instance;
 	//CONNECTION SETTINGS
 	public static final int PORT = 54321;
-	public static final String HOST = "192.168.1.106";
+	public static final String HOST = "localhost";
 	//FILE SETTINGS
 	public static final String DATA_PATH = "data/";
 	public static final String USERS_PATH = DATA_PATH+"users/";
